3a15ea0c3bcc2e2b7bc0e8e807ba07b887be04eb
chartdesign <chartshorne@gmail.com>
2023-11-27 15:08:32 -0800

    added sidebar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,66 +1,15 @@
-import React, { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { FaSearch, FaRegWindowClose } from "react-icons/fa"; // Import the search icon from react-icons
+import Sidebar from "./SideBar";
 
 const Header = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
   return (
     <nav className='bg-lightGreen p-4 '>
       <div className='container mx-auto flex items-center justify-between'>
-        <div className='text-white text-2xl font-bold'>
-          <span>Your Logo</span>
-        </div>
+        <div className='flex'>Your Logo</div>
 
         {/* Hamburger Menu Icon (visible on small screens) */}
         <div className='lg:hidden'>
-          <GiHamburgerMenu
-            className='text-white text-3xl cursor-pointer'
-            onClick={toggleMobileMenu}
-          />
+          <Sidebar />
         </div>
-
-        {/* Mobile Navigation Menu (hidden on larger screens) */}
-        {isMobileMenuOpen && (
-          <div className='lg:hidden absolute inset-0 bg-white z-50  ease-in-out duration-300'>
-            <div className='container mx-auto flex items-center justify-between p-4'>
-              <div className='text-midGreen text-2xl font-bold'>
-                <span>Your Logo</span>
-              </div>
-              <FaRegWindowClose
-                className='text-midGreen text-3xl cursor-pointer'
-                onClick={toggleMobileMenu}
-              />
-            </div>
-
-            <div className='flex flex-col items-center justify-center h-full space-y-4'>
-              <a href='#' className='text-midGreen text-xl hover:text-gray-400'>
-                Link 1
-              </a>
-              <a href='#' className='text-midGreen text-xl hover:text-gray-400'>
-                Link 2
-              </a>
-              <a href='#' className='text-midGreen text-xl hover:text-gray-400'>
-                Link 3
-              </a>
-              <div className='flex items-center space-x-4'>
-                {/* Search Text Box with Search Icon */}
-                <input
-                  type='text'
-                  placeholder='Search...'
-                  className='bg-gray-700 text-white px-3 py-1 rounded-md w-full flex-1'
-                />
-                <button className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600'>
-                  <FaSearch className='text-xl' />
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
 
         {/* Desktop Navigation Links (hidden on small screens) */}
         <div className='hidden lg:flex space-x-4'>
